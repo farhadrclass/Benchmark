e1fc00330bd5276dbd5ca031285525aef12ab570
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2023-01-18 10:02:08 -0500

    cleaning the code

--- R2/benchmark_R2_momentum.jl
+++ R2/benchmark_R2_momentum.jl
@@ -3,6 +3,7 @@ using LinearAlgebra
 using Random
 using Printf
 using DataFrames
+using CSV
 using OptimizationProblems
 using NLPModels
 using ADNLPModels
@@ -27,13 +28,17 @@ problems =
 solvers = Dict(
   :R2_Momentum_3 => nlp -> R2(
     nlp;
+    max_time=60.0,
     # max_eval = 10,
+    atol=T(0.0001),
     β = T(0.3), #testing the momentum 0.9
   ),
-  :R2 => nlp -> R2(nlp),
+  :R2 => nlp -> R2(nlp;atol=T(0.0001),    max_time=60.0  ),
   :R2_Momentum_9 => nlp -> R2(
     nlp;
+    atol=T(0.0001),
     # max_eval = 10,
+    max_time=60.0,
     β = T(0.9), #testing the momentum 0.9
   ),
 )
@@ -71,6 +76,21 @@ for solver ∈ keys(solvers)
   pretty_stats(stats[solver][!, cols], hdr_override = header)
 end
 
+@eval getname(::Type{<:$T}) = $T
+
+#write to file 
+for solver ∈ keys(solvers)
+  s = "R2//Result//"*string( getname(T))*string(solver)
+  open(s*".txt","w") do io
+    pretty_stats(io,stats[solver][!, cols], hdr_override = header)
+  end
+  open(s*".tex","w") do io
+    latex_table(io,stats[solver][!, cols], hdr_override = header)
+  end
+end
+  
+
+
 first_order(df) = df.status .== :first_order
 unbounded(df) = df.status .== :unbounded
 solved(df) = first_order(df) .| unbounded(df)
@@ -85,3 +105,4 @@ using Plots
 gr()
 
 profile_solvers(stats, costs, costnames)
+
